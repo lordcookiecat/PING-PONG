02973e42e26c599092d8882ef59ee8951122fde6
Lauri Matterne <LMatterne@intershop.com>
2024-06-07 11:35:37 +0200

    Initial pong project.

---
+++ src/spiel/PongGame.java
@@ -0,0 +1,16 @@
+package spiel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+
+public class PongGame {
+
+	public static void main(String[] args) {
+		
+	  GameFrame frame = new GameFrame();
+	  
+	}
+
+}
